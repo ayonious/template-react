735826a676b99fec02b555b0b56a3ffe9e0c7bf5
Nahiyan Kamal <noyanayihan@gmail.com>
2020-04-24 13:06:08 +0200

    first working commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Project Template
+
+This repo contains a bare minimum react Project. Without using create-react-app
+
+This contains:
+
+1. Babbel
+2. Webpack
+3. React
---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <h1>Making it work</h1>;
+  }
+}
+
+export default Dashboard;
